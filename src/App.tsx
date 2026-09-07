25e3c7b0bd56285b65f726c4c36c825341d67421
alexh5427 <alexander.m.higgins@maine.edu>
2026-09-07 12:51:44 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): React.JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload Alexander Higgins.
+                automatically reload. Hello World this is Alexander Higgins.
             </p>
         </div>
     );
