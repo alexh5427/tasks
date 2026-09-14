b483d3226e4baf47df1cfb32851bb6f2648a6499
alexh5427 <alexander.m.higgins@maine.edu>
2026-09-14 17:35:24 -0400

    Complete HTML and CSS task

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,61 @@
+
 import React from "react";
 import "./App.css";
+import { Button, Container, Row, Col } from "react-bootstrap";
 
-function App(): React.JSX.Element {
+function App() {
     return (
         <div className="App">
             <header className="App-header">
-                UM COS420 with React Hooks and TypeScript
+
+                <h1>UM COS420 with React Hooks and TypeScript</h1>
+
+                <img
+                    src="https://via.placeholder.com/150"
+                    alt="Placeholder"
+                />
+
+                <ul>
+                    <li>React</li>
+                    <li>TypeScript</li>
+                    <li>JavaScript</li>
+                </ul>
+
+                <Button
+                    className="btn btn-primary"
+                    onClick={() => console.log("Hello World!")}
+                >
+                    Log Hello World
+                </Button>
+
+                <Container>
+                    <Row>
+                        <Col>
+                            <div
+                                style={{
+                                    width: "100px",
+                                    height: "100px",
+                                    backgroundColor: "red"
+                                }}
+                            ></div>
+                        </Col>
+
+                        <Col>
+                            <div
+                                style={{
+                                    width: "100px",
+                                    height: "100px",
+                                    backgroundColor: "red"
+                                }}
+                            ></div>
+                        </Col>
+                    </Row>
+                </Container>
+
             </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Hello World this is Alexander Higgins.
-            </p>
         </div>
     );
 }
 
 export default App;
+
